1a04b5d5ffb729d43c6fad6862065fa1771197d5
Horas Hajovan Siregar <45363813+horasjey@users.noreply.github.com>
2026-07-17 15:35:33 +0700

    Update health check response message

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 
 export const createHealthController = () => {
   const app = new Hono().basePath("/health").get("/", async (c) => {
-    return c.json({ status: "ok" });
+    return c.json({ status: "ok update horas" });
   });
   return app;
 };
